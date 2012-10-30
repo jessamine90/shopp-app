c10c711633e04a1417bf58cc7ff8fc02e451cd4b
jessamine90@gmail.com <jessamine90@gmail.com>
2012-10-30 14:59:40 +0100

    typo

--- Gemfile
+++ Gemfile
@@ -10,7 +10,6 @@ gem 'devise'
 gem 'faker'
 gem 'ransack'
 
-
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,5 @@
 class OrderItem < ActiveRecord::Base
-	atrr_accesible :product_id, :quantity, :price, :order_id
+	attr_accesible :product_id, :quantity, :price, :order_id
 	belongs_to :order
 	belongs_to :product
 	
