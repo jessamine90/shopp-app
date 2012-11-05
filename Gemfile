6362f4631754c0f9639ecc57684ec34cc7f6f628
jessamine90@gmail.com <jessamine90@gmail.com>
2012-11-05 17:47:02 +0100

    working state just before homework 4, nothing for homework 3

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'devise'
 gem 'faker'
 gem 'ransack'
 
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,7 @@
 ShopApp::Application.routes.draw do
-
   root :to => "shop#index"
-  match 'search/results' => "search#results"
-  resource :shop
   resources :products
   resources :categories
+
   
-  resources :orders do
-    collection do
-      post 'finalize'
-      post 'add_product'
-      get 'confirm'
-    end
-  end
-  
-  resource :cart do
-    post 'dodaj'
-	put 'complete'
-  end
-  
-  namespace :admin do
-	devise_for :admin_users
-	resources :products
-	resources :categories
-	root :to => "orders#index"
-  end
 end
